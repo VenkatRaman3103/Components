073da95eb02eeec99a482cfb614922c475250df5
venkatraman3103 <venkat.raman.3103@gmail.com>
2024-10-08 20:13:54 +0530

    integrate dummy data with UI

--- task-manager/src/Components/TaskManager.tsx
+++ task-manager/src/Components/TaskManager.tsx
@@ -1,7 +1,112 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './TaskManager.scss'
 
+const dummyData = [
+    {
+        id: 1,
+        title: 'Personal Task',
+        color: '#FFCC00', // Yellow
+        task: {
+            type: 'toDo',
+            tasks: [
+                {
+                    content: 'lorem ipsum',
+                    isChecked: false
+                }
+            ]
+        }
+    },
+    {
+        id: 2,
+        title: 'Team Meeting',
+        color: '#00CCFF', // Blue
+        task: {
+            type: 'meeting',
+            tasks: [
+                {
+                    members: [{ name: 'name1', img: 'url' }],
+                    notes: ['lorem ipsum', 'lorem ipsum']
+                }
+            ]
+        }
+    },
+    {
+        id: 3,
+        title: 'Project Report',
+        color: '#FF6600', // Orange
+        task: {
+            type: 'toDo',
+            tasks: [
+                {
+                    content: 'Finish project report',
+                    isChecked: true
+                }
+            ]
+        }
+    },
+    {
+        id: 4,
+        title: 'Grocery Shopping',
+        color: '#66FF66', // Green
+        task: {
+            type: 'toDo',
+            tasks: [
+                {
+                    content: 'Buy groceries',
+                    isChecked: false
+                }
+            ]
+        }
+    },
+    {
+        id: 5,
+        title: 'Project Milestones Discussion',
+        color: '#FF0099', // Pink
+        task: {
+            type: 'meeting',
+            tasks: [
+                {
+                    members: [
+                        { name: 'name2', img: 'url2' },
+                        { name: 'name3', img: 'url3' }
+                    ],
+                    notes: ['Discuss project milestones', 'Allocate tasks']
+                }
+            ]
+        }
+    },
+    {
+        id: 6,
+        title: 'Weekly Sync-Up',
+        color: '#6600FF', // Purple
+        task: {
+            type: 'meeting',
+            tasks: [
+                {
+                    members: [{ name: 'name4', img: 'url4' }],
+                    notes: ['Weekly sync-up', 'Review previous actions']
+                }
+            ]
+        }
+    },
+    {
+        id: 7,
+        title: 'Client Feedback Call',
+        color: '#FF3333', // Red
+        task: {
+            type: 'toDo',
+            tasks: [
+                {
+                    content: 'Call the client for feedback',
+                    isChecked: false
+                }
+            ]
+        }
+    }
+];
+
 const TaskManager = () => {
+    const [tasks, setTasks] = useState(dummyData)
     return (
         <div className='container'>
             <div className='wrapper'>
@@ -85,12 +190,12 @@ const TaskManager = () => {
                 </div>
                 <div className='upcoming-tasks-section'>
                     <div className='upcoming-tasks-wrapper'>
-                        {Array.from({ length: 6 }).map((item, index) =>
+                        {tasks?.map((item, index) =>
                             <div className='task-component'>
                                 <div className='accent-strip'></div>
                                 <div className='task-overview-wrapper'>
                                     <div className='task-timing'>12:30 PM - 01:00 PM</div>
-                                    <div className='task-name'>Meeting with Person {index}</div>
+                                    <div className='task-name'>{item.title} - {item.task.type}</div>
                                 </div>
                             </div>
                         )}
