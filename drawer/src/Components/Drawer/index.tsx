c3fdadd95ca5f5c211e2931c85a17fca2f9eccee
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2025-02-20 19:48:12 +0530

    added dummy data and updated ui

--- drawer/src/Components/Drawer/index.tsx
+++ drawer/src/Components/Drawer/index.tsx
@@ -3,6 +3,56 @@
 import React, { useState } from "react";
 import "./index.scss";
 
+type objectType = {
+    type: "app" | "link" | "notes" | "search";
+    label: string;
+    content?: {
+        icon: string;
+        heading?: string;
+        description: string;
+    }[];
+};
+
+type dataType = objectType[];
+
+const data: dataType = [
+    {
+        type: "app",
+        label: "App",
+        content: [
+            {
+                icon: "red",
+                description: "incididunt ut labore et dolore magna aliqua",
+            },
+        ],
+    },
+    {
+        type: "link",
+        label: "Links",
+        content: [
+            {
+                icon: "red",
+                description: "incididunt ut labore et dolore magna aliqua",
+            },
+        ],
+    },
+    {
+        type: "notes",
+        label: "Notes",
+        content: [
+            {
+                icon: "red",
+                heading: "heading",
+                description: "incididunt ut labore et dolore magna aliqua",
+            },
+        ],
+    },
+    {
+        type: "search",
+        label: "Search",
+    },
+];
+
 export const Drawer = () => {
     const [isMouseEnter, setIsMouseEnter] = useState<boolean>(true);
 
@@ -12,7 +62,7 @@ export const Drawer = () => {
                 <div
                     className={`options-tray-container ${isMouseEnter ? "expand" : ""}`}
                     // onMouseEnter={() => setIsMouseEnter(true)}
-                    // onMouseLeave={() => setIsMouseEnter(true)}
+                    // onMouseLeave={() => setIsMouseEnter(false)}
                 >
                     <div
                         className={`content-wraper ${isMouseEnter ? "open" : ""}`}
@@ -20,14 +70,16 @@ export const Drawer = () => {
                         <div className="divider"></div>
                     </div>
                     <div className="options-tray-wrapper">
-                        <div className="option">App</div>
-                        <div className="option">App</div>
-                        <div className="option">App</div>
-                        <div className="option">App</div>
-                        <div className="option">App</div>
+                        {data.map((item: objectType, ind: number) => (
+                            <Button key={ind} label={item.label} />
+                        ))}
                     </div>
                 </div>
             </div>
         </div>
     );
+};
+
+const Button = ({ label }: { label: string }) => {
+    return <div className="option">{label}</div>;
 };
